d5d2bf2fd31eecbf055710912494d1fb2f6c5c68
mateusjdev <git@mateusj.dev>
2025-12-30 18:17:15 -0300

    Add go build information to --version

--- cmd/version.go
+++ cmd/version.go
@@ -8,13 +8,16 @@ import (
 
 const ApplicationName = "Scruffy"
 
-var ApplicationVersion = "build from source"
+var (
+	ApplicationVersion = "build from source"
+	GoBuildVersion     = "unknown"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of scruffy",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s - %s", ApplicationName, ApplicationVersion)
+		fmt.Printf("%s - %s\n%s\n", ApplicationName, ApplicationVersion, GoBuildVersion)
 	},
 }
 
